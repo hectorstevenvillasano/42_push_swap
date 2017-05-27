b6c69dd535df655afbc58232af8e001821de6d27
hvillasa <hectorstevenvillasano@outlook.com>
2017-05-26 21:46:24 -0700

    checker

---
+++ checker.c
@@ -0,0 +1,45 @@
+#include "push_swap.h"
+
+
+
+static int set_features(t_stack *stack, char **av)
+{
+	if (!ft_strcmp(av[1], "-v"))
+		stack->features = 1;
+	else if (!ft_strcmp(av[1], "-c"))
+		stack->features = 2;
+	else if (!ft_strcmp(av[2], "-cv") || !ft_strcmp(av[2], "-vc"))
+		stack->features = 3;
+	else
+		return (0);
+	return (1);
+}
+
+int main(int ac, char **av)
+{
+	char	*input;
+	t_stack	*stack;
+
+	stack = initstack();
+	if (ac == 1)
+	{
+		ft_putchar('\n');
+		return (0);
+	}
+	if (av[1][0] == '-')
+		if (set_features(stack, av) && ac--)
+			av++;
+	if (create_stack(ac, av, &stack)) //| solver(&stack))
+	{
+		write(2, "Error\n", 6);
+		return (0);
+	}
+	stack->b = init_array(stack->size_a);
+	while (get_next_line(0, &input) > 0)
+	{
+		apply_operations(stack, input);
+		free(input);
+	}
+	sort_check(stack) ? ft_putendl("OK") : ft_putendl("KO");
+	return (0);
+}
